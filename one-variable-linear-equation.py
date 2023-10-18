8cad5a3d860ce2162e34cf5b3d33572d5a61806e
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-17 22:42:04 -0400

    Update one-variable-linear-equation.py

--- one-variable-linear-equation.py
+++ one-variable-linear-equation.py
@@ -1,16 +1,16 @@
 import numpy as np
 import tensorflow as tf
 
-# List 1 (X-values, 1000 data points)
+# list 1 (X-values)
 x_values_list = [i for i in range(1, 100_000)]
 X = np.array(x_values_list)
 
-# List 2 (Y-values, corresponding to the function f(x), 1000 data points)
+# list 2 (Y-values, corresponding to the function f(x))
 # f(x) = 2x + 3
 y_values_list = [2*x + 3 for x in x_values_list]
 y = np.array(y_values_list)
 
-# Build and compile your model
+# build and compile model
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(1,)),
     tf.keras.layers.Dense(32, activation='relu'),
@@ -19,13 +19,12 @@ model = tf.keras.Sequential([
 
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-# Train the model
+# train the model
 model.fit(X, y, epochs=100, batch_size=64, validation_split=0.2)
 
 print("--Training Complete--")
 
-# Make predictions
-
+# make predictions
 while True: 
 	guess = float(input(" f(x)=2x+3 >> "))
 	print( model.predict(np.array([guess])) )
