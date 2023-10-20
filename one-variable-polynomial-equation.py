9a31f49e428f2c728cf36cab9c62e15041437d0b
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-20 09:56:42 -0400

    Update one-variable-polynomial-equation.py

--- one-variable-polynomial-equation.py
+++ one-variable-polynomial-equation.py
@@ -1,1 +1,29 @@
+import numpy as np
+import tensorflow as tf
 
+# input list (X-values)
+x_values_list = [i for i in range(1, 100_000)]
+X = np.array(x_values_list)
+
+# output/answers list (Y-values, corresponding to the function f(x))
+# f(x) = (x^2)+2x+3
+y_values_list = [(x^2)+2x+ 3 for x in x_values_list]
+Y = np.array(y_values_list)
+
+# build and compile model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(1,)),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# train the model
+model.fit(X, Y, epochs=50, batch_size=64, validation_split=0.2)
+
+print("--Training Complete--")
+
+# make predictions
+while True: 
+	guess = float(input(" f(x)=2x+3 >> "))
+	print( model.predict(np.array([guess])) )
